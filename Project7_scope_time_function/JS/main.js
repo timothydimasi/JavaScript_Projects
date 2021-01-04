dd437aabfa32eae12c7b03f959bace297fae0a14
timothydimasi <timothydimasi@gmail.com>
2021-01-03 20:27:58 -0800

    Project7 Commit

---
+++ Project7_scope_time_function/JS/main.js
@@ -0,0 +1,45 @@
+function add_one() {
+    var x = 5;
+    document.write(x + 1 + "<br>");
+}
+
+function add_two() {
+    var x = 5;
+    document.write(x + 2 + "<br>");
+}
+
+add_one();
+add_two();
+
+
+function date_test() {
+    if (new Date().getHours() < 24 ) {
+        document.getElementById("date").innerHTML = "It's not midnight yet.";
+    }
+}
+
+function ski_trip() {
+    Trips = document.getElementById("ski_trips").value;
+    if (Trips >= 8) {
+        Amount = "That's a lot of ski trips!";
+    }
+    else {
+        Amount = "That's not that many ski trips.";
+    }
+    document.getElementById("lot_or_little").innerHTML = Amount;
+}
+
+function Time_Function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time.";
+    }
+    else if (Time > 12 == Time < 18) {
+        Reply = "It is afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
